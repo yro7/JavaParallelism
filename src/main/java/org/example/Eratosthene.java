ae46126c8cc0cb87ce65141daaebdda4b990b7d5
Yronusa <yronusa2000@gmail.com>
2025-01-28 11:17:45 +0100

    Optimized Eratosthene

--- src/main/java/org/example/Eratosthene.java
+++ src/main/java/org/example/Eratosthene.java
@@ -1,54 +1,42 @@
 package org.example;
 
-import java.time.chrono.Era;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+import java.util.BitSet;
 import java.util.function.Consumer;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Eratosthene {
+    private final BitSet nonPrimes;
+    private final int bound;
 
-    private HashSet<Integer> nonPrimes;
-    private final Integer bound;
+    public static final Consumer<Integer> eratosthene = Eratosthene::new;
 
-    public static final Consumer<Integer> eratosthene = n -> new Eratosthene(n).compute();
-
-    public Eratosthene(int n){
+    public Eratosthene(int n) {
         this.bound = n;
-        this.nonPrimes = new HashSet<>();
+        this.nonPrimes = new BitSet(n);
+        compute();
     }
 
     public int[] compute() {
-        IntStream integerStream = IntStream.range(2,this.bound);
-        int[] res = integerStream.filter(this::isPrimeSmart)
-                        .toArray();
-        System.out.print("number of primes between 1 and " + bound + " : " + res.length);
-        return res;
-    }
+        // Mark 0 and 1 as non-prime
+        nonPrimes.set(0);
+        nonPrimes.set(1);
 
-    private boolean isPrimeSmart(int integer) {
-        if(nonPrimes.contains(integer)){
-            return false;
-        }
-
-        int i = 2;
-        while(i*integer < bound){
-            nonPrimes.add(integer*i);
-            i++;
-        }
-
-        return isPrime(integer);
-    }
-
-    private static boolean isPrime(int integer){
-        for(int i = 2; i < integer; i++){
-            if (integer % i == 0){
-                return false;
+        // Main sieve algorithm
+        for (int i = 2; i <= Math.sqrt(bound); i++) {
+            if (!nonPrimes.get(i)) {
+                // Mark all multiples of i as non-prime
+                for (int j = i * i; j < bound; j += i) {
+                    nonPrimes.set(j);
+                }
             }
         }
-        return true;
+
+        // Create array of prime numbers using stream
+        int[] primes = IntStream.range(0, bound)
+                .filter(i -> !nonPrimes.get(i))
+                .toArray();
+
+        System.out.printf("Number of primes between 1 and %d: %d%n", bound, primes.length);
+        return primes;
     }
 }
